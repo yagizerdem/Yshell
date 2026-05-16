20a643e5e3567ba47e3093e13ce36d2c0e0f0474
yagiz erdem <2yagiz004@gmail.com>
2026-05-16 22:30:04 +0300

    globber added

--- src/main/java/ysh/Main.java
+++ src/main/java/ysh/Main.java
@@ -1,55 +1,12 @@
 package ysh;
 
 
-import ysh.natives.*;
-import ysharp.treewalk.evaluator.Interpreter;
-import ysharp.treewalk.lexer.Lexer;
-import ysharp.treewalk.lexer.Preprocess;
-import ysharp.treewalk.lexer.Token;
-import ysharp.treewalk.parser.Parser;
-
-import java.util.List;
+import java.nio.file.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
 
-        String code = """
-                   pipe("where java", "findstr java");
-                """;
-
-        Lexer lexer = new Lexer(Preprocess.removeComments(Preprocess.mergeContinuation(code)));
-        List<Token> tokenStream = lexer.scanTokens();
-
-        if(lexer.hadErrors()) {
-            lexer.errors.forEach(x -> {
-                System.out.println(x.getMessage());
-            });
-        }
-
-        Parser parser = new Parser(tokenStream);
-        Parser.Program program = parser.parse();
-
-        if(parser.hadErrors()) {
-            parser.errors.forEach(x -> {
-                System.out.println(x.getMessage());
-            });
-        }
-
-        Interpreter interpreter = new Interpreter();
-
-        Register.nativeFunction(interpreter, new Run());
-        Register.nativeFunction(interpreter, new Exec());
-        Register.nativeFunction(interpreter, new ExecRaw());
-        Register.nativeFunction(interpreter, new Pipe());
-
-        interpreter.interpret(program.program);
-
-        if(interpreter.hadErrors()) {
-            interpreter.errors.forEach(x -> {
-                System.out.println(x.getMessage());
-            });
-        }
-
+        REPL.start();
 
 
     }
--- src/main/java/ysh/REPL.java
+++ src/main/java/ysh/REPL.java
@@ -1,5 +1,55 @@
 package ysh;
 
+import ysh.natives.*;
+import ysharp.treewalk.evaluator.Interpreter;
+import ysharp.treewalk.lexer.Lexer;
+import ysharp.treewalk.lexer.Preprocess;
+import ysharp.treewalk.lexer.Token;
+import ysharp.treewalk.parser.Parser;
+
+import java.util.List;
+
 public class REPL {
 
+    public static void start() throws Exception {
+        String code = """
+                   var list = glob("**/*.ysh");
+                   println list.size();
+                   println list.toString();
+                """;
+
+        Lexer lexer = new Lexer(Preprocess.removeComments(Preprocess.mergeContinuation(code)));
+        List<Token> tokenStream = lexer.scanTokens();
+
+        if(lexer.hadErrors()) {
+            lexer.errors.forEach(x -> {
+                System.out.println(x.getMessage());
+            });
+        }
+
+        Parser parser = new Parser(tokenStream);
+        Parser.Program program = parser.parse();
+
+        if(parser.hadErrors()) {
+            parser.errors.forEach(x -> {
+                System.out.println(x.getMessage());
+            });
+        }
+
+        Interpreter interpreter = new Interpreter();
+
+        Register.nativeFunction(interpreter, new Run());
+        Register.nativeFunction(interpreter, new Exec());
+        Register.nativeFunction(interpreter, new ExecRaw());
+        Register.nativeFunction(interpreter, new Pipe());
+        Register.nativeFunction(interpreter, new Glob());
+
+        interpreter.interpret(program.program);
+
+        if(interpreter.hadErrors()) {
+            interpreter.errors.forEach(x -> {
+                System.out.println(x.getMessage());
+            });
+        }
+    }
 }
